6671f18b0b12050d100208c695026ed7219ea969
Agon Ajdini <150683579+aa30142@users.noreply.github.com>
2024-11-18 18:49:37 +0100

    Added skybox. Need to add textures

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js' 
 
 const scene = new THREE.Scene();
 
-const cameraPers = new THREE.PerspectiveCamera(45, 4/3);
+const cameraPers = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,45,30000);
 
-const skyBoxGeometry = new THREE.BoxGeometry(5,5);
+const skyBoxGeometry = new THREE.BoxGeometry(10000,10000,10000);
 const skyBoxMaterial = new THREE.MeshBasicMaterial({
-    color: 0xff0000
+    color: 0xffffff
 });
 const skyBox = new THREE.Mesh(skyBoxGeometry,skyBoxMaterial);
 
-const renderer = new THREE.WebGLRenderer();
-document.getElementById("scene").appendChild(renderer.domElement);
+scene.add(skyBox);
 
-renderer.render(scene,cameraPers);
+cameraPers.position.z = 10;
+cameraPers.position.x = 3;
+
+scene.add(cameraPers);
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(cameraPers, renderer.domElement);
+controls.enableDamping = true;
+controls.target = skyBox.position;
+
+function animate(){
+    renderer.render(scene,cameraPers);
+    controls.update();
+    requestAnimationFrame(animate);
+}
+
+animate();
+
