782c25540adc4f6bf0b35b5badc9913bc141bd8b
Agon Ajdini <150683579+aa30142@users.noreply.github.com>
2024-11-19 21:04:02 +0100

    Made pink cube spin!

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js' 
+import { metalness } from 'three/webgpu';
+import gsap from 'gsap'
 
 const scene = new THREE.Scene();
 
-const cameraPers = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,45,30000);
+const cameraPers = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,45,150000);
 
 const textureLoader = new THREE.TextureLoader();
 
@@ -27,7 +29,7 @@ materialarray.push(new THREE.MeshBasicMaterial({map:txLeft,side:THREE.BackSide})
 
 
 
-const skyBoxGeometry = new THREE.BoxGeometry(20000,20000,20000);
+const skyBoxGeometry = new THREE.BoxGeometry(100000,100000,100000);
 
 // const skyBoxMaterial = new THREE.MeshBasicMaterial({
 //     map: texture,
@@ -51,7 +53,7 @@ const height = textureLoader.load("textures/Tiles_053_SD/Tiles_053_height.png");
 const normal = textureLoader.load("textures/Tiles_053_SD/Tiles_053_normal.png");
 const rough = textureLoader.load("textures/Tiles_053_SD/Tiles_053_roughness.png"); 
 
-const planeBoxGeometry = new THREE.BoxGeometry(4000,4000,1);
+const planeBoxGeometry = new THREE.BoxGeometry(10000,10000,1);
 const planeBoxMaterial = new THREE.MeshStandardMaterial({
     aOMap: aO,
     map: col,
@@ -73,6 +75,22 @@ scene.add(spotLight);
 const spotHelp = new THREE.DirectionalLightHelper(spotLight);
 scene.add(spotHelp);
 
+const cubeGeometry = new THREE.BoxGeometry(200,200,200);
+const cubeMaterial = new THREE.MeshStandardMaterial({
+    color: 0xff51cf,
+    roughnes: 0,
+    metalness: 0.387,
+    fog:1
+})
+
+const cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
+cube.position.y += 200;
+cube.position.x = 3000;
+
+gsap.to(cube.rotation, { duration: 3, y: Math.PI * 2, z: Math.PI*2, ease: "linear", repeat: -1, yoyo: false });
+
+scene.add(cube);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -81,6 +99,8 @@ const controls = new OrbitControls(cameraPers, renderer.domElement);
 controls.enableDamping = true;
 controls.minDistance = 500;
 controls.maxDistance = 3000;
+
+controls.target = cube.position;
 
 function animate(){
     renderer.render(scene,cameraPers);
