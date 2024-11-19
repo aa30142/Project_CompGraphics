d4651bb109191e85dd47826b4bd473ab266a438a
Agon Ajdini <150683579+aa30142@users.noreply.github.com>
2024-11-19 01:02:29 +0100

    Added Skybox. SUGGESTION: MAKE TEXTURES SMOOTHER

--- script.js
+++ script.js
@@ -3,13 +3,37 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const scene = new THREE.Scene();
 
-const cameraPers = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,45,30000);
+const cameraPers = new THREE.PerspectiveCamera(65, window.innerWidth/window.innerHeight,45,30000);
+
+const textureLoader = new THREE.TextureLoader();
+
+const materialarray = [];
+
+let txBack = textureLoader.load("textures/Daylight Box_Pieces/Daylight Box_Back.png");
+let txBottom = textureLoader.load("textures/Daylight Box_Pieces/Daylight Box_Bottom.png");
+let txFront = textureLoader.load("textures/Daylight Box_Pieces/Daylight Box_Front.png");
+let txLeft = textureLoader.load("textures/Daylight Box_Pieces/Daylight Box_Left.png");
+let txRight = textureLoader.load("textures/Daylight Box_Pieces/Daylight Box_Right.png");
+let txTop = textureLoader.load("textures/Daylight Box_Pieces/Daylight Box_Top.png");
+
+materialarray.push(new THREE.MeshBasicMaterial({map:txFront,side:THREE.BackSide}));
+materialarray.push(new THREE.MeshBasicMaterial({map:txBack,side:THREE.BackSide}));
+materialarray.push(new THREE.MeshBasicMaterial({map:txTop,side:THREE.BackSide}));
+materialarray.push(new THREE.MeshBasicMaterial({map:txBottom,side:THREE.BackSide}));
+materialarray.push(new THREE.MeshBasicMaterial({map:txRight,side:THREE.BackSide}));
+materialarray.push(new THREE.MeshBasicMaterial({map:txLeft,side:THREE.BackSide}));
+
+
+
+
 
 const skyBoxGeometry = new THREE.BoxGeometry(10000,10000,10000);
-const skyBoxMaterial = new THREE.MeshBasicMaterial({
-    color: 0xffffff
-});
-const skyBox = new THREE.Mesh(skyBoxGeometry,skyBoxMaterial);
+
+// const skyBoxMaterial = new THREE.MeshBasicMaterial({
+//     map: texture,
+//     side: THREE.BackSide
+// });
+const skyBox = new THREE.Mesh(skyBoxGeometry,materialarray);
 
 scene.add(skyBox);
 
