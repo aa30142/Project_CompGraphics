b4972363058173eac59b8bbfe470bf09cd4585ad
Agon Ajdini <150683579+aa30142@users.noreply.github.com>
2025-02-13 11:04:59 +0100

    Accuracy values (pointer.x and pointer.y) should be fine now.

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ const SelectObject = (event) =>
 
     //Numbers given are for accuracy of the raycast (may need to adjust these values based on aspect ratio or other variables rather than these values).
     pointer.x = (event.clientX / window.innerWidth)*2 - 1.008;
-    pointer.y = -(event.clientY / window.innerHeight)*2 + 0.943;
+    pointer.y = -(event.clientY / window.innerHeight)*2 + 1.0178;
 
     console.log("x: " +pointer.x + " y: " + pointer.y);
 
