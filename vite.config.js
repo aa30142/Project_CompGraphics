6a8f542db9ff00d2254bbfc573a6733eadf7ac62
Agon Ajdini <150683579+aa30142@users.noreply.github.com>
2025-02-19 11:49:11 +0100

    small changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-    base: '/CompGraph/',
+    base: '/your-repo-name/',
     build: {
         outDir: 'dist',
         sourcemap: true,
